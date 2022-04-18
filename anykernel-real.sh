612c315c20d8b8269a9f1a9e4ef69ba11e8e3269
pingmado <75196272+ping2109@users.noreply.github.com>
2022-04-18 15:17:23 +0700

    Update anykernel-real.sh

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -4,24 +4,24 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=NevaCore by ping2109official@TG
+kernel.string=NevaCore by AnhNotAnh@TG
 kernel.for=your mom
-kernel.compiler=GitHub Actions
+kernel.compiler=Proton Clang
 kernel.made=
-kernel.version=
+kernel.version=1.0
 message.word=Enjoy the #NevaPower
 build.date=2021
-do.devicecheck=0
+do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
+device.name1=flashlmdd
 device.name2=
 device.name3=
 device.name4=
 device.name5=
-supported.versions=
+supported.versions=9-12
 supported.patchlevels=
 '; } # end properties
 
